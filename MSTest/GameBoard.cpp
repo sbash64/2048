a7e7853cdca56d03653c5742a8893bfd436c22ae
sbash64 <sbash64@gmail.com>
2018-08-04 11:37:25 -0500

    pass

--- MSTest/GameBoard.cpp
+++ MSTest/GameBoard.cpp
@@ -13,4 +13,10 @@ const std::vector<std::vector<double>>& GameBoard::getBoard()
 
 void GameBoard::moveRight()
 {
+	for (size_t i = 0; i < board.size(); i++)
+		for (size_t j = 0; j < board[i].size(); j++)
+			if (board[i][j] == 2) {
+				board[i].back() = 4;
+				board[i][j] = 0;
+			}
 }
