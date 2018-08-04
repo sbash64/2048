26da6b4546bbb8995ca8177027bb174ed4d595ec
sbash64 <sbash64@gmail.com>
2018-08-04 13:31:56 -0500

    pass

--- Game/GameBoard.cpp
+++ Game/GameBoard.cpp
@@ -14,12 +14,32 @@ void GameBoard::moveRight()
 {
 	for (size_t i = 0; i < board.size(); i++)
 	{
-		double sum = 0;
-		for (size_t j = 0; j < board[i].size()-1; j++)
+		for (int j = board[i].size()-2; j > -1; j--)
 		{
-			sum += board[i][j];
-			board[i][j] = 0;
+			if (board[i][j] != 0)
+			{
+				size_t k = j;
+				while (k < board[i].size()-1 && board[i][++k] == 0)
+					;
+				if (board[i][k] == 0)
+				{
+					double value = board[i][j];
+					board[i][j] = 0;
+					board[i][k] = value;
+				}
+				else if (board[i][k] == board[i][j])
+				{
+					double value = board[i][j];
+					board[i][j] = 0;
+					board[i][k] = 2 * value;
+				}
+				else
+				{
+					double value = board[i][j];
+					board[i][j] = 0;
+					board[i][k-1] = value;
+				}
+			}
 		}
-		board[i].back() = sum;
 	}
 }
