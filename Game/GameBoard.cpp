da2f98a7ada11c24f0e2cd6840db04148918c1bc
sbash64 <sbash64@gmail.com>
2018-08-04 13:37:45 -0500

    refactor

--- Game/GameBoard.cpp
+++ Game/GameBoard.cpp
@@ -12,34 +12,22 @@ const std::vector<std::vector<double>>& GameBoard::getBoard()
 
 void GameBoard::moveRight()
 {
-	for (size_t i = 0; i < board.size(); i++)
-	{
-		for (int j = board[i].size()-2; j > -1; j--)
+	for (int i = 0; i < board.size(); i++)
+		for (int j = board[i].size() - 2; j > -1; j--)
 		{
-			if (board[i][j] != 0)
+			double value = board[i][j];
+			if (value != 0)
 			{
-				size_t k = j;
-				while (k < board[i].size()-1 && board[i][++k] == 0)
+				int k = j;
+				while (k < board[i].size() - 1 && board[i][++k] == 0)
 					;
 				if (board[i][k] == 0)
-				{
-					double value = board[i][j];
-					board[i][j] = 0;
 					board[i][k] = value;
-				}
-				else if (board[i][k] == board[i][j])
-				{
-					double value = board[i][j];
-					board[i][j] = 0;
+				else if (board[i][k] == value)
 					board[i][k] = 2 * value;
-				}
 				else
-				{
-					double value = board[i][j];
-					board[i][j] = 0;
-					board[i][k-1] = value;
-				}
+					board[i][k - 1] = value;
+				board[i][j] = 0;
 			}
 		}
-	}
 }
