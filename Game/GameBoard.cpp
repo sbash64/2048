2c6201d7ccd7bd0d8b52a9cc48330effe6f0ed23
sbash64 <sbash64@gmail.com>
2018-08-05 15:29:44 -0500

    probably more inefficient

--- Game/GameBoard.cpp
+++ Game/GameBoard.cpp
@@ -15,28 +15,25 @@ void GameBoard::moveRight()
 	for (int row = 0; row < board.size(); row++)
 	{
 		std::vector<bool> hasBeenCombined(board[row].size(), false);
-		const auto oneBeforeEnd = board[row].size() - 2;
-		for (int col = oneBeforeEnd; col > -1; col--)
+		const auto columnBeforeEnd = board[row].size() - 2;
+		for (int col = columnBeforeEnd; col > -1; col--)
 		{
 			const double value = board[row][col];
-			if (value != 0)
+			const auto nextNonzeroOrLastColumn = getNextNonzeroOrLastColumn(row, col);
+			if (board[row][nextNonzeroOrLastColumn] == 0)
+				board[row].back() = value;
+			else if (
+				board[row][nextNonzeroOrLastColumn] == value &&
+				!hasBeenCombined[nextNonzeroOrLastColumn])
 			{
-				const auto nextNonzeroOrLastColumn = getNextNonzeroOrLastColumn(row, col);
-				if (board[row][nextNonzeroOrLastColumn] == 0)
-					board[row].back() = value;
-				else if (
-					board[row][nextNonzeroOrLastColumn] == value &&
-					!hasBeenCombined[nextNonzeroOrLastColumn])
-				{
-					board[row][nextNonzeroOrLastColumn] = 2 * value;
-					hasBeenCombined[nextNonzeroOrLastColumn] = true;
-				}
-				else if (nextNonzeroOrLastColumn != col + 1)
-					board[row][nextNonzeroOrLastColumn - 1] = value;
-				else
-					continue;
-				board[row][col] = 0;
+				board[row][nextNonzeroOrLastColumn] += value;
+				hasBeenCombined[nextNonzeroOrLastColumn] = true;
 			}
+			else if (nextNonzeroOrLastColumn != col + 1)
+				board[row][nextNonzeroOrLastColumn - 1] = value;
+			else
+				continue;
+			board[row][col] = 0;
 		}
 	}
 }
