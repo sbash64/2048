ea12f2eec00d0757efc7b7f08e57f251d506d945
sbash64 <sbash64@gmail.com>
2018-09-01 13:43:35 -0500

    pass

--- MSTest/test_board_utility.cpp
+++ MSTest/test_board_utility.cpp
@@ -4,5 +4,9 @@
 std::vector<std::vector<double>> rotateClockwise(
 	std::vector<std::vector<double>> board)
 {
-	return board;
+	auto rotated(board);
+	for (size_t row = 0; row < rotated.size(); row++)
+		for (size_t col = 0; col < rotated[row].size(); col++)
+			rotated[row][col] = (*(board.end() - col - 1))[row];
+	return rotated;
 }
