50b00e584034142750a47a4f9daa83b3139caa1c
sbash64 <sbash64@gmail.com>
2018-09-01 13:47:39 -0500

    free test

--- MSTest/TestBoardTester.cpp
+++ MSTest/TestBoardTester.cpp
@@ -45,5 +45,23 @@ namespace MSTest
 						{ 0, 0, 0, 0 }
 					}));
 		}
+
+		TEST_METHOD(testRotateClockwiseUniqueElements)
+		{
+			assertAreEqual(
+				{
+					{ 13, 9, 5, 1 },
+					{ 14, 10, 6, 2 },
+					{ 15, 11, 7, 3 },
+					{ 16, 12, 8, 4 }
+				},
+				rotateClockwise(
+					{
+						{ 1, 2, 3, 4 },
+						{ 5, 6, 7, 8 },
+						{ 9, 10, 11, 12 },
+						{ 13, 14, 15, 16 }
+					}));
+		}
 	};
 }
