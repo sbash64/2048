7012054da84f5e15339431ef4226a6d57d70cdbd
sbash64 <sbash64@gmail.com>
2018-09-15 18:41:14 -0500

    fail

---
+++ MSTest/GameBoardFormatterTester.cpp
@@ -0,0 +1,30 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+namespace MSTest
+{
+	TEST_CLASS(GameBoardFormatterTester)
+	{
+	public:
+		TEST_METHOD(testInitialDisplay)
+		{
+			using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+			Assert::AreEqual(
+				"0 0 0 0\n" \
+				"0 0 0 0\n" \
+				"0 0 0 0\n" \
+				"0 0 0 0",
+				GameBoardFormatter{}.as_string(
+					GameBoard(
+						{
+							{ 0, 0, 0, 0 },
+							{ 0, 0, 0, 0 },
+							{ 0, 0, 0, 0 },
+							{ 0, 0, 0, 0 }
+						}
+					)
+				));
+			);
+		}
+	};
+}
