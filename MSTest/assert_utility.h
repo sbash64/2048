c8f49c8f164f7638d9f6da668c91c996da24447d
sbash64 <sbash64@gmail.com>
2018-09-16 12:21:17 -0500

    refactor

--- MSTest/assert_utility.h
+++ MSTest/assert_utility.h
@@ -1,9 +1,30 @@
 #pragma once
+#include "stdafx.h"
+#include "CppUnitTest.h"
 #include <vector>
 
+template<typename T>
 void assertAreEqual(
-	const std::vector<std::vector<double>> &expected,
-	const std::vector<std::vector<double>> &actual);
+	const std::vector<T> &expected,
+	const std::vector<T> &actual)
+{
+	for (size_t i = 0; i < expected.size(); ++i)
+		Microsoft::VisualStudio::CppUnitTestFramework::Assert::AreEqual(
+			expected[i],
+			actual[i]
+		);
+}
+
+template<typename T>
 void assertAreEqual(
-	const std::vector<size_t> &expected,
-	const std::vector<size_t> &actual);
+	const std::vector<std::vector<T>>& expected,
+	const std::vector<std::vector<T>>& actual)
+{
+	for (size_t i = 0; i < expected.size(); i++)
+		for (size_t j = 0; j < expected[i].size(); j++)
+			Microsoft::VisualStudio::CppUnitTestFramework::Assert::AreEqual(
+				expected[i][j],
+				actual[i][j]
+			);
+
+}
