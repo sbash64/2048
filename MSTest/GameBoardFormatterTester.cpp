eb6f895af2275d8fda1bda6df71fbd15f478c547
sbash64 <sbash64@gmail.com>
2018-09-16 16:33:10 -0500

    expose another failed test

--- MSTest/GameBoardFormatterTester.cpp
+++ MSTest/GameBoardFormatterTester.cpp
@@ -26,6 +26,21 @@ namespace MSTest
 					)
 				).c_str());
 			Assert::AreEqual(
+				"0 0 0 2\n" \
+				"0 0 0 0\n" \
+				"0 0 0 0\n" \
+				"0 0 0 0",
+				GameBoardFormatter{}.asString(
+					GameBoard(
+						{
+							{ 0, 0, 0, 2 },
+							{ 0, 0, 0, 0 },
+							{ 0, 0, 0, 0 },
+							{ 0, 0, 0, 0 }
+						}
+					)
+				).c_str());
+			Assert::AreEqual(
 				" 1  2  3  4\n" \
 				" 5  6  7  8\n" \
 				" 9 10 11 12\n" \
