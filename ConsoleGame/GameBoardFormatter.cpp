22cdadcf40013dde36db8452e4c88e2a599bc97d
sbash64 <sbash64@gmail.com>
2018-09-16 11:33:12 -0500

    pass

--- ConsoleGame/GameBoardFormatter.cpp
+++ ConsoleGame/GameBoardFormatter.cpp
@@ -1,15 +1,36 @@
 #include "GameBoardFormatter.h"
 #include <sstream>
+#include <algorithm>
+
+static int digits(int number)
+{
+	int _digits = 0;
+	while (number) {
+		number /= 10;
+		_digits++;
+	}
+	return _digits;
+}
 
 std::string GameBoardFormatter::asString(const GameBoard & board)
 {
 	std::stringstream stream;
 	const auto _board = board.getBoard();
+	std::vector<int> maxDigitsInColumn;
+	for (size_t col = 0; col < _board.front().size(); ++col)
+	{
+		double max = 0;
+		for (size_t row = 0; row < _board.size(); ++row)
+			max = std::max(max, _board[row][col]);
+		maxDigitsInColumn.push_back(digits(max));
+	}
 	for (size_t row = 0; row < _board.size(); ++row)
-		for (size_t col = 0; col < _board[row].size(); ++col)
+		for (size_t col = 0; col < _board.front().size(); ++col)
 		{
+			for (int x = 0; x < maxDigitsInColumn[col] - digits(_board[row][col]); ++x)
+				stream << " ";
 			stream << _board[row][col];
-			if (col == _board[row].size() - 1)
+			if (col == _board.front().size() - 1)
 				if (row == _board.size() - 1)
 					;
 				else
