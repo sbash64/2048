f36551b2e0fa3129e558b43a6d8728b654655280
sbash64 <sbash64@gmail.com>
2018-09-16 17:09:03 -0500

    fail

--- MSTest/ConsoleGameControllerTester.cpp
+++ MSTest/ConsoleGameControllerTester.cpp
@@ -228,5 +228,30 @@ namespace MSTest
 				"0 0 0 0",
 				device->lastOutput().c_str());
 		}
+
+		TEST_METHOD(testNextWithNoArrowKey)
+		{
+			const auto device = std::make_shared<MockIODevice>();
+			ConsoleGameController controller(
+				GameBoard(
+					{
+						{ 0, 0, 0, 0 },
+						{ 0, 0, 0, 0 },
+						{ 0, 0, 0, 0 },
+						{ 0, 2, 0, 0 }
+					}
+				),
+				std::make_shared<GameBoardFormatter>(),
+				device);
+			controller.next();
+			Microsoft::VisualStudio::CppUnitTestFramework::Assert::AreEqual(
+				"Unrecognized key pressed. Press an arrow key to play.\n" \
+				"\n" \
+				"0 0 0 0\n" \
+				"0 0 0 0\n" \
+				"0 0 0 0\n" \
+				"0 2 0 0",
+				device->lastOutput().c_str());
+		}
 	};
 }
