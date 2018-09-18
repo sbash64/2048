58c77a0619cf3c6949d3cdc35d78ae456c1ab23f
sbash64 <sbash64@gmail.com>
2018-09-17 23:12:06 -0500

    mersenne

--- Console2048/MersenneTwisterGenerator.cpp
+++ Console2048/MersenneTwisterGenerator.cpp
@@ -1,6 +1,12 @@
 #include "MersenneTwisterGenerator.h"
 
+MersenneTwisterGenerator::MersenneTwisterGenerator() :
+	rng(std::random_device{}())
+{
+}
+
 int MersenneTwisterGenerator::randomIntBetween(int low, int hi)
 {
-	return 0;
+	std::uniform_int_distribution<int> uni(low, hi);
+	return uni(rng);
 }
--- Console2048/MersenneTwisterGenerator.h
+++ Console2048/MersenneTwisterGenerator.h
@@ -1,8 +1,12 @@
 #pragma once
 
 #include <RandomNumberGenerator.h>
+#include <random>
 
 class MersenneTwisterGenerator : public RandomNumberGenerator
 {
+	std::mt19937 rng;
+public:
+	MersenneTwisterGenerator();
 	virtual int randomIntBetween(int low, int hi) override;
 };
