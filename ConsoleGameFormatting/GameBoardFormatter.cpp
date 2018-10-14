d0ec6266bf1325443985abbdb1a9499e35e00960
sbash64 <sbash64@gmail.com>
2018-10-14 15:16:11 -0500

    std::size_t

--- ConsoleGameFormatting/GameBoardFormatter.cpp
+++ ConsoleGameFormatting/GameBoardFormatter.cpp
@@ -18,8 +18,8 @@ std::string GameBoardFormatter::asString(const GameBoard &board)
 {
 	std::stringstream stream;
 	const auto maxDigits = maxDigitsInEachColumn(board);
-	for (size_t row = 0; row < board.size(); ++row)
-		for (size_t col = 0; col < board.size(); ++col)
+	for (std::size_t row = 0; row < board.size(); ++row)
+		for (std::size_t col = 0; col < board.size(); ++col)
 		{
 			for (
 				int i = 0; 
@@ -43,10 +43,10 @@ std::vector<int> GameBoardFormatter::maxDigitsInEachColumn(
 	const GameBoard &board)
 {
 	std::vector<int> maxDigits;
-	for (size_t col = 0; col < board.size(); ++col)
+	for (std::size_t col = 0; col < board.size(); ++col)
 	{
 		double max = 0;
-		for (size_t row = 0; row < board.size(); ++row)
+		for (std::size_t row = 0; row < board.size(); ++row)
 			max = std::max(max, board[row][col]);
 		maxDigits.push_back(digits(max));
 	}
--- Game/GameBoardAnalyzer.cpp
+++ Game/GameBoardAnalyzer.cpp
@@ -1,10 +1,10 @@
 #include "GameBoardAnalyzer.h"
 
-std::vector<size_t> GameBoardAnalyzer::openCells(const GameBoard &board)
+std::vector<std::size_t> GameBoardAnalyzer::openCells(const GameBoard &board)
 {
-	std::vector<size_t> cells;
-	for (size_t col = 0; col < board.size(); ++col)
-		for (size_t row = 0; row < board.size(); ++row)
+	std::vector<std::size_t> cells;
+	for (std::size_t col = 0; col < board.size(); ++col)
+		for (std::size_t row = 0; row < board.size(); ++row)
 			if (board[row][col] == 0)
 				cells.push_back(row + col * board.size());
 	return cells;
@@ -12,8 +12,8 @@ std::vector<size_t> GameBoardAnalyzer::openCells(const GameBoard &board)
 
 bool GameBoardAnalyzer::canMove(const GameBoard &board)
 {
-	for (size_t col = 0; col < board.size(); ++col)
-		for (size_t row = 0; row < board.size(); ++row)
+	for (std::size_t col = 0; col < board.size(); ++col)
+		for (std::size_t row = 0; row < board.size(); ++row)
 			if (board[row][col] == 0)
 				return true;
 	return false;
