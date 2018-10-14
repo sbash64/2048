e699e9ee83a5732e12ea98ab65f5724f52dc36f6
sbash64 <sbash64@gmail.com>
2018-10-14 15:30:39 -0500

    pass

--- Game/GameBoardAnalyzer.cpp
+++ Game/GameBoardAnalyzer.cpp
@@ -12,9 +12,20 @@ std::vector<std::size_t> GameBoardAnalyzer::openCells(const GameBoard &board)
 
 bool GameBoardAnalyzer::canMove(const GameBoard &board)
 {
-	for (std::size_t col = 0; col < board.size(); ++col)
-		for (std::size_t row = 0; row < board.size(); ++row)
+	for (std::size_t row = 0; row < board.size(); ++row)
+		for (std::size_t col = 0; col < board.size(); ++col) {
 			if (board[row][col] == 0)
 				return true;
+			if (
+				col < board.size() - 1 && 
+				board[row][col] == board[row][col + 1]
+			)
+				return true;
+			if (
+				row < board.size() - 1 &&
+				board[row][col] == board[row + 1][col]
+			)
+				return true;
+		}
 	return false;
 }
