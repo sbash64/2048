1640b72569a771363052d177a9d43440d7c6cb2f
sbash64 <sbash64@gmail.com>
2018-11-23 15:38:26 -0600

    lost the colors but back to passing

--- MSTest/assert_utility.cpp
+++ MSTest/assert_utility.cpp
@@ -1,8 +1,8 @@
 #include "assert_utility.h"
 
 void assertAreEqual(
-	const std::vector<std::vector<double>>& expected, 
-	const GameBoard & actual)
+	const std::vector<std::vector<double>> &expected, 
+	const GameBoard &actual)
 {
 	for (std::size_t i = 0; i < expected.size(); i++)
 		assertAreEqual(expected[i], actual[i]);
